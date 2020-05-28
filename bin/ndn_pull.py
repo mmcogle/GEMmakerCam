b54959fa975155dd1a14ed97ab2f5ed1526100ea
mmcogle <cam@localhost.localdomain>
2020-05-27 21:03:20 -0400

    Updated script

--- bin/ndn_pull.py
+++ bin/ndn_pull.py
@@ -5,39 +5,47 @@ import subprocess
 import sys
 import argparse
 
-def create_face():
+def create_face(nameSpace):
     output_file = open("face_information", "w+")
-    nameSpace = "udp://atmos-nwsc.ucar.edu"
     subprocess.call(["nfdc","face","create",nameSpace], stdout=output_file)
-    output_file.close()
-    output_file = open("face_information", "r")
-    face_info = output_file.read()
-    face_info = face_info[12:]
-    mydict = dict((k.strip(), v.strip()) for k,v in 
-              (item.split('=') for item in face_info.split(' ')))
-    print(mydict["id"])
-    print("done")
-
+    output_file.close()    
+    print("Face Created")
     
+def add_route(nameSpace,route):
+    output_file = open("route_information","w+")
+    subprocess.call(["nfdc","route","add",route,nameSpace],stdout=output_file)
+    output_file.close()
+    print("Route Added")
 
-
-def get_from_ndn(line):
-    os.chdir("/Users/cameronogle/Documents/Research/GEMmakerCam/input")
-    output_file = open("Arabidopsis.sra", "w+")
-    name = "/Arabidopsis/thaliana/rnaseq/"
-    name += line
-    print(name)
+def get_from_ndn():
+    output_file = open("Homo_sapiens_1.fastq", "w+")
+    name = "/BIOLOGY/SRA/9605/9606/NaN/RNA-Seq/ILLUMINA/TRANSCRIPTOMIC/PAIRED/Kidney/PRJNA359795/SRP095950/SRX2458154/SRR5139395/SRR5139395_1"
     subprocess.call(["ndncatchunks",name], stdout=output_file)
+    print("Downloaded file")
+    
    # subprocess.call(["fasterq-dump", "Arabidopsis.sra"])
 
 
 
 if __name__ == "__main__":
+
+    subprocess.call(["nfd-status"])
+    repo_domain = "atmos-csu.research-lan.colostate.edu"
+    proto = "tcp4://"
+    route = "/BIOLOGY"
+    nameSpace = proto+repo_domain
+    try:
+        create_face(nameSpace)
+        add_route(nameSpace,route)
+        get_from_ndn()
+    except:
+        sys.exit(1)
+    sys.exit(1)
     # parse command-line arguments
-    parser = argparse.ArgumentParser()
-    parser.add_argument("SRR_ID_FILE", help="SRA run ID file")
-    args = parser.parse_args()
-    create_face()
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument("SRR_ID_FILE", help="SRA run ID file")
+    # args = parser.parse_args()
+
     # load SRA run IDs
     # srr_file = open(args.SRR_ID_FILE, "r")
     # for line in srr_file:
